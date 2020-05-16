628caced6ca6fd100a9d5f33dd9e988069c31c6d
alvin <alvin.leung92@gmail.com>
2020-05-16 16:14:02 -0700

    Enable authorization in PostsController

--- Tweetbook/Controllers/V1/PostsController.cs
+++ Tweetbook/Controllers/V1/PostsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Threading.Tasks;
 using Tweetbook.Contract.V1;
@@ -9,6 +11,7 @@ using Tweetbook.Services;
 
 namespace Tweetbook.Controllers.V1
 {
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class PostsController : Controller
     {
         private readonly IDataService<Post> postService;
